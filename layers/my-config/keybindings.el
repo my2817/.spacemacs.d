d1e1d8316d2bdf5fd5702c3fafa6b54b2d0b680d
my2817 <my2817@163.com>
2020-12-16 14:01:24 +0800

    [window] "spc w p r" bind to toggle-window-split

--- layers/my-config/keybindings.el
+++ layers/my-config/keybindings.el
@@ -21,4 +21,4 @@
 (spacemacs/set-leader-keys "m)" 'awesome-pair-unwrap)
 (spacemacs/set-leader-keys "m<" 'awesome-pair-jump-left)
 (spacemacs/set-leader-keys "m>" 'awesome-pair-jump-right)
-(spacemacs/set-leader-keys "wpr" 'window-toggle-split-direction)
+(spacemacs/set-leader-keys "wpr" 'toggle-window-split)
