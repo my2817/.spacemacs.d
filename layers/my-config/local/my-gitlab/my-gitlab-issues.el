58ea369cc27b6414c66da54faeb7cc7c46412219
my2817 <my2817@163.com>
2022-11-12 15:18:45 +0800

    fix(gitlab,issue): refresh list or create new one

--- layers/my-config/local/my-gitlab/my-gitlab-issues.el
+++ layers/my-config/local/my-gitlab/my-gitlab-issues.el
@@ -1,5 +1,8 @@
 (require 'gitlab)
 
+(define-key gitlab-issues-mode-map (kbd "R") 'gitlab-issues-refresh)
+(define-key gitlab-issues-mode-map (kbd "c") 'gitlab-issues-create)
+(define-key gitlab-issues-mode-map (kbd "u") 'gitlab-issues-modi)
 ;; duplicate define funcs
 (define-derived-mode gitlab-issues-mode tabulated-list-mode "Gitlab issues"
   "Major mode for browsing Gitlab issues."
@@ -37,5 +40,128 @@
   (interactive)
   (gitlab-issues-for-project (gitlab-get-project-id-by-name (projectile-project-name)))
   )
+
+;; save project to local
+(defun gitlab-issues-for-project (&optional project-id)
+  "From projects buffer, opens issues buffer for project at point."
+  (interactive)
+  (let ((project (gitlab-get-project (or project-id (tabulated-list-get-id)))))
+    (if project
+	(progn
+	  (pop-to-buffer "*Gitlab issues*" nil)
+          (make-local-variable 'gitlab-project)
+	  (gitlab-issues-mode)
+          (setq-local gitlab-project project)
+	  (setq tabulated-list-entries
+		(create-issues-entries (gitlab-list-project-issues (assoc-default 'id project))))
+	  (tabulated-list-print t)
+	  (tabulated-list-sort 1))
+      (user-error "No project here"))))
+
+(defun gitlab-issues-refresh ()
+  "refresh current project's issues list"
+  (interactive)
+  (gitlab-issues-for-project (assoc-default 'id gitlab-project)))
+
+(defun gitlab-issues-create ()
+  "create a new issue"
+  (interactive)
+  (let* ((project-id (assoc-default 'id gitlab-project)))
+    (gitlab-issue-edit project-id nil)
+    ))
+
+(defun gitlab-issues-do-put ()
+  "DO `gitlab-issue-put'"
+  (interactive)
+  (let ()
+    (gitlab-issues-put (assoc-default 'project-id gitlab-info)
+                       (gitlab-issue-parse-buffer)
+                       (assoc-default 'issue-id gitlab-info))
+    (kill-buffer-and-window)))
+
+(defun gitlab-issues-modi ()
+  "Edit issue's title/assignee/description at cursor"
+  (interactive)
+  (let* ((project-id (assoc-default 'project_id (tabulated-list-get-id)))
+         (issue-iid (assoc-default 'iid (tabulated-list-get-id))) )
+    (gitlab-issue-edit project-id issue-iid)
+    )
+  )
+
+(defun gitlab-issues-put (project_id params &optional iid)
+  "create a new issue"
+  (perform-gitlab-request (if iid "PUT" "POST")
+                          (s-concat "projects/"
+                                    (url-hexify-string
+                                     (format "%s" project_id))
+                                    "/issues"
+                                    (and iid
+                                         (format "/%s" iid)))
+                          params
+                          (if iid 200 201) )
+  )
+(defun gitlab-issue-edit (&optional project-id issue-id)
+  "create or edit a issue.
+
+PROJECT-ID: project-id
+ISSUE-ID: nil to create a new ISSUE; or update a issue"
+  (let* (
+         (issue (and issue-id
+                     (gitlab-get-issue project-id issue-id)))
+         (title (or (and issue-id
+                         (assoc-default 'title issue))
+                    (format "TITLE")))
+         (description (or (and issue-id
+                               (assoc-default 'description issue))
+                          (format "DESCRIPTION")))
+         (assignee (or (and issue-id
+                            (assoc-default 'name (assoc-default 'assignee issue)))
+                       (format "ASSIGNEE")))
+         (gitlab-info )
+         )
+    (pop-to-buffer (format "*Gitlab Edit Issue:[#%s]*" (or issue-id
+                                                          "new")) nil)
+    (gitlab-edit-note-mode)
+    (use-local-map (let ((map (copy-keymap markdown-mode-map)))
+                     ;; (define-key map (kbd "v") 'print-current-issue-id)
+                     ;; (define-key map (kbd "w") 'gitlab-goto-issue)
+                     (define-key map (kbd "C-c C-c" ) 'gitlab-issues-do-put)
+                     map) )
+    (setq-local gitlab-project-id project-id)
+    (page-break-lines-mode)
+    (setq header-line-format
+          (concat (propertize " " 'display '((space :align-to 0)))
+                  (format "[%s] C-c C-c: finish Edit; Kill Buffer: Abort\n\nMarkDown: Don't remove TAG and line-break"
+                          (projectile-project-name))))
+    (make-local-variable 'gitlab-info)
+    (setq-local gitlab-info (list (cons 'project-id project-id)
+                                  (cons 'issue-id issue-id)))
+    (dolist (item (list 'title 'assignee 'description))
+            (insert (format "%s:\n=\n%s\n\n\n" item (eval item))))
+    ))
+
+(defun gitlab-issue-parse-buffer ()
+  "Collection issue information in buffer which created by `gitlab-issue-edit'"
+  (goto-char (point-min))
+  (let* ((title-st (progn
+                     (search-forward "title:\n=\n" (point-max))
+                     (point)
+                     ))
+         (title (buffer-substring-no-properties (point) (point-at-eol)))
+         (assignee-st (progn
+                        (search-forward "assignee:\n=\n" (point-max))
+                        (point)
+                        ))
+         (assignee (buffer-substring-no-properties (point) (point-at-eol)))
+         (description-st (progn
+                           (search-forward "description:\n=\n" (point-max))
+                           (point)
+                           ))
+         (description (buffer-substring-no-properties (point) (- (point-max) 2)))
+         )
+    (list (cons 'title title)
+          (cons 'assignee_ids (gitlab-get-user-id-by-username assignee))
+          (cons 'description description))
+    ))
 
 (provide 'my-gitlab-issues)
