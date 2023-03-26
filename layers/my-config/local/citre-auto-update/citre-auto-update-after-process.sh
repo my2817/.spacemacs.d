8084b3f26d27394160df7973806a8b4ec7a8fc28
my2817 <my2817@163.com>
2023-03-26 17:21:10 +0800

    fix(citre-auto-update): replace tagsfile only when no other proces access it

--- layers/my-config/local/citre-auto-update/citre-auto-update-after-process.sh
+++ layers/my-config/local/citre-auto-update/citre-auto-update-after-process.sh
@@ -10,4 +10,15 @@ echo TAG_PROC_CWD: ${origin_TAG_PROC_CWD}
 sed -i "1i ${origin_CITRE_CMD}" ${new_tagsfile}
 sed -i "/\!_TAG_PROC_CWD/d" ${new_tagsfile}
 sed -i "1i ${origin_TAG_PROC_CWD}" ${new_tagsfile}
+
+# replace file when no process use the tagsfile
+proc_num=$(lsof -t ${old_tagsfile} |wc -l)
+while [ $proc_num -gt 0 ]
+do
+    echo  Some process is reading ${old_tagsfile}, will check it again after 1 second
+    sleep 1
+    proc_num=$(lsof -t ${old_tagsfile} |wc -l)
+done
+
+
 mv ${new_tagsfile} ${old_tagsfile}
